8ba980cffefb85d55f01e30101ad6881fe7dff75
Marco Dallagiacoma <marco.dallagiacoma@gmail.com>
2024-06-07 16:14:57 +0200

    Start POC

---
+++ tests/test_better_betterproto.py
@@ -0,0 +1,10 @@
+from tests.output_betterproto.simple import Test
+
+from tests.output_reference.simple import simple_pb2
+
+
+# def test_serializes_correctly():
+#     serialized = bytes(Test(field=123))
+#     google_serialized = simple_pb2.Test(field=123).SerializeToString()
+
+# assert google_serialized == serialized
